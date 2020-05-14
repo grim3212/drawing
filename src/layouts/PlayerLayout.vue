a81e0286b986f37b41a78df7ae5cd2d972710797
Dakota Pierone <dakotapierone@yahoo.com>
2020-05-14 06:40:38 -0700

    Route guard

--- src/layouts/PlayerLayout.vue
+++ src/layouts/PlayerLayout.vue
@@ -29,14 +29,13 @@ export default {
     timerColor() {
       return this.timer <= 10 ? 'text-negative' : 'text-positive'
     }
-  }
-  /*,
+  },
   beforeRouteEnter(to, from, next) {
     next(vm => {
       if (!vm.$socket.connected()) return next({ name: 'join' })
       else return next()
     })
-  }*/
+  }
 }
 </script>
 
